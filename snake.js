58aa2861530b078f9818f41536c61ea6a91730f7
stevula <brodericksteven@gmail.com>
2016-03-10 14:03:00 -0800

    Snake can eat apples and grow

--- snake.js
+++ snake.js
@@ -1,19 +1,8 @@
-var drawSnake = function(snake) {
+var draw = function(snake, apple) {
     var drawableSnake = {color: "green", pixels: snake},
-        drawableObjects = [drawableSnake];
+        drawableApple = {color: "red", pixels: [apple]},
+        drawableObjects = [drawableSnake, drawableApple];
     CHUNK.draw(drawableObjects);
-};
-
-var moveSnake = function(snake) {
-    var oldSegment = snake[0],
-        newSegment = moveSegment(oldSegment);
-    newSegment.direction = oldSegment.direction;
-    return [newSegment];
-};
-
-var advanceGame = function() {
-    snake = moveSnake(snake);
-    drawSnake(snake);
 };
 
 var moveSegment = function(segment) {
@@ -31,8 +20,49 @@ var moveSegment = function(segment) {
     }
 };
 
-var snake = [{top: 1, left: 1, direction: "down"}],
-    apple = [{top: 5, left: 10}],
-    drawableApple = {color: "red", pixels: apple};
+var moveSnake = function(snake) {
+    return snake.map(function(oldSegment, segmentIndex) {
+        var newSegment = moveSegment(oldSegment);
+        newSegment.direction = segmentFartherForwardThan(segmentIndex, snake).direction;
+        return newSegment;
+    });
+};
+
+var advanceGame = function() {
+    snake = moveSnake(snake);
+    if (CHUNK.detectCollisionBetween([apple], snake)) {
+        snake = growSnake(snake);
+        apple = CHUNK.randomLocation();
+    }
+    if (CHUNK.detectCollisionBetween(snake, CHUNK.gameBoundaries())) {
+        CHUNK.endGame();
+        CHUNK.flashMessage("You died.");
+    }
+    draw(snake, apple);
+};
+
+var segmentFartherForwardThan = function(index, snake) {
+    if (snake[index - 1] === undefined) {
+        return snake[index];
+    }
+    else {
+        return snake[index - 1];
+    }
+};
+
+var changeDirection = function(direction) {
+    snake[0].direction = direction;
+};
+
+var growSnake = function(snake) {
+    var indexOfLastSegment = snake.length - 1,
+        lastSegment = snake[indexOfLastSegment];
+    snake.push({top: lastSegment.top, left: lastSegment.left});
+    return snake;
+};
+
+var snake = [{top: 1, left: 1, direction: "down"}, {top: 0, left: 1, direction: "down"}],
+    apple = {top: 5, left: 10};
 
 CHUNK.executeNTimesPerSecond(advanceGame, 2);
+CHUNK.onArrowKey(changeDirection);
